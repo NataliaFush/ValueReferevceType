1e4f9c671d3de35d66cd5ea149d8a1483b5ec656
NatalliaF <natfushtey@gmail.com>
2023-09-20 14:53:43 +0300

    Add project files.

---
+++ ValueReferevceType/MemotyAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueReferevceType
+{
+    public static class MemoryAddress
+    {
+        public static unsafe string Get(object a)
+        {
+            TypedReference tr = __makeref(a);
+            IntPtr ptr = **(IntPtr**)(&tr);
+            return "0x" + ptr.ToString("X");
+        }
+
+        public static unsafe string Get(ref object a)
+        {
+            TypedReference tr = __makeref(a);
+            IntPtr ptr = **(IntPtr**)(&tr);
+            return "0x" + ptr.ToString("X");
+        }
+    }
+}
---
+++ ValueReferevceType/Program.cs
@@ -0,0 +1,31 @@
+﻿using ValueReferevceType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car1 = new Car("blue");
+        object car = car1;
+
+        Console.WriteLine("Address car1         "+MemoryAddress.Get(car1));
+        Console.WriteLine("Address ref car=car1 " + MemoryAddress.Get(ref car));
+
+        int a = 7;
+        object b = a;
+
+        Console.WriteLine("Address int a       " + MemoryAddress.Get(a));
+        Console.WriteLine("Address ref int b=a " + MemoryAddress.Get(ref b));
+
+        string str1 = "MyStr";
+        object str2 = str1;
+
+        Console.WriteLine("Address str1         " + MemoryAddress.Get(str1));
+        Console.WriteLine("Address ref srt2=str1" + MemoryAddress.Get(ref str2));
+
+
+
+
+
+
+    }
+}
